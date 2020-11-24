a5d2313b6bf80aca095c1587ad9baddf2a941ff6
Amon <j.niedzw@gmail.com>
2020-11-24 23:43:49 +0100

    [Upload] Setup script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+apt update && apt install -y python3 python3-dev python3-pip python3-venv nano curl wget rsync git zsh
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+pip3 install virtualenv Pygments
+
+sed -i 's@ZSH_THEME="robbyrussell"@ZSH_THEME="powerlevel10k/powerlevel10k"@g' ~/.zshrc
+sed -i 's@plugins=(git)@plugins=(git extract colorize cp rsync pip virtualenv zsh-autosuggestions zsh-syntax-highlighting)@g' ~/.zshrc
+
+echo "alias edt='nano ~/.zshrc'" >> ~/.zshrc
+echo "alias edt='nano ~/.zshrc'" >> ~/.zshrc
+echo "alias cls='clear && printf \"\e[3J\"'" >> ~/.zshrc
