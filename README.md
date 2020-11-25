8e1c4ab297f6d3a5446530a46e9a32388d4ac39d
Amon <j.niedzw@gmail.com>
2020-11-25 01:49:07 +0100

    [Core] Change to bash

--- README.md
+++ README.md
@@ -2,18 +2,18 @@
 
 Install and configure zsh environment. Single script which:
 - installs brew (for MacOS only)
-- installs packages (``python3 python3-venv nano curl wget rsync git zsh``)
+- installs packages (``python3 python3-dev python3-pip python3-venv nano curl wget rsync git zsh``)
 - sets default shell to ``zsh``
 - installs [Oh My Zsh](https://github.com/ohmyzsh/ohmyzsh)
 - adds plugin [zsh-autosuggestions](https://github.com/zsh-users/zsh-autosuggestions)
 - adds plugin [zsh-syntax-highlighting](https://github.com/zsh-users/zsh-syntax-highlighting)
 - sets theme [Powerlevel10k](https://github.com/romkatv/powerlevel10k)
 - installs python pip packages (``virtualenv pygments``)
-- adds some custom commands (``edt src cls``)
+- adds some custom aliases (``edt src cls ll``)
 
 # Install
 
 ```bash
 apt update && apt install -y curl
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/moozeq/ZSH_Setup/master/setup.sh)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/moozeq/ZSH_Setup/master/setup.sh)"
 ```
--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-if [ "$OSTYPE" == "linux-gnu"* ]; then
-    apt update && apt install -y python3 python3-venv nano curl wget rsync git zsh
-elif [ "$OSTYPE" == "darwin"* ]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    apt update && apt install -y python3 python3-dev python3-pip python3-venv nano curl wget rsync git zsh sudo
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     brew update && brew install python3 curl wget rsync git zsh
 else
     echo "[-] OS $OSTYPE not recognized, abort"
@@ -17,7 +17,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HO
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-pip3 install virtualenv pygments
+pip3 install --user virtualenv pygments
 
 sed -i 's@ZSH_THEME="robbyrussell"@ZSH_THEME="powerlevel10k/powerlevel10k"@g' ~/.zshrc
 sed -i 's@plugins=(git)@plugins=(git extract colorize cp rsync pip virtualenv zsh-autosuggestions zsh-syntax-highlighting)@g' ~/.zshrc
@@ -25,5 +25,7 @@ sed -i 's@plugins=(git)@plugins=(git extract colorize cp rsync pip virtualenv zs
 echo "alias edt='nano ~/.zshrc'" >> ~/.zshrc
 echo "alias src='source ~/.zshrc'" >> ~/.zshrc
 echo "alias cls='clear && printf \"\e[3J\"'" >> ~/.zshrc
+echo "alias ll='ls -hla'" >> ~/.zshrc
+echo "export PATH=\"$PATH:$HOME/.local/bin\"" >> ~/.zshrc
 
 zsh
